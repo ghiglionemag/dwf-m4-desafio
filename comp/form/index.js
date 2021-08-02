c769308aca8ea37b2b2020fa195e0fb7b0610ada
unknown <ghiglionemag@gmail.com>
2021-08-02 16:53:21 -0300

    Last commit?

--- comp/form/index.js
+++ comp/form/index.js
@@ -1,34 +1,53 @@
 function contactComponent(el) {
-  const componentEl = document.createElement("section");
+  const componentEl = document.createElement("div");
 
   componentEl.innerHTML = `
 
-    <section class="form-container">
-      
+    <div class="form-container">
+    <h2 class="form__title">Escribime</h2>
       <form class="form-content">
-      <h2 class="form__title">Escribime</h2>
-      <label>
+      
+      <label  for="nombre" name="nombre">
           <h3 class="form__label">Nombre</h3>
-          <input class="form__input" type="text" />
+          <input class="form__input" type="text" id="nombre"/>
         </label>
-        <label>
+        <label  for="email" name="email">
           <h3 class="form__label">Email</h3>
-          <input class="form__input" type="text" />
+          <input class="form__input" type="text" id="email"/>
         </label>
-        <label>
+        <label  for="mensaje" name="mensaje">
           <h3 class="form__label">Mensaje</h3>
-          <input class="form__input" type="text-area" />
+          <input class="form__input-text" type="text-area" id="mensaje" />
         </label>
         <div class="form__submit-cont">
           <button class="form__submit-button">Enviar</button>
         </div>
       </form>
-      </section>
+      </div>
     `;
 
-  const form = componentEl.querySelector(".form-container");
-  form.addEventListener("submit", function (e) {
+  const formEl = componentEl.querySelector(".form-content");
+  formEl.addEventListener("submit", function (e) {
     e.preventDefault();
+    const form = e.target;
+    const objeto = {
+      nombre: form.nombre.value,
+      email: form.email.value,
+      mensaje: form.mensaje.value,
+    };
+    fetch("https://apx-api.vercel.app/api/utils/dwf ", {
+      method: "POST",
+      body: JSON.stringify({
+        to: objeto.email,
+        message: objeto.mensaje,
+      }),
+      headers: { "content-type": "application/json" },
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      redirect: "follow",
+      referrerPolicy: "no-referrer",
+    });
   });
 
   el.appendChild(componentEl);
