fb91137d488772fa5c80b3ada1bb884bb96c890f
ghiglionemag <76672510+ghiglionemag@users.noreply.github.com>
2021-08-06 14:24:00 -0300

    Update footer.js

--- comp/footer/footer.js
+++ comp/footer/footer.js
@@ -3,7 +3,7 @@ function footerComponent(el) {
   componentEl.innerHTML = `
   <div class="footer__content">
   <img class="footer__logo" src="img/logo.png" alt="Logo agus" />
-  <div class="footer__content-link">
+  <div class="footer__links">
   <a
     href="https://www.instagram.com/ghiglioneagus/?hl=es-la"
     class="footer__link"
@@ -13,8 +13,7 @@ function footerComponent(el) {
       alt="Icono Instagram"
     />Instagram</a
   >
-  </div>
-  <div class="footer__content-link">
+  
   <a
     href="https://www.linkedin.com/in/agustina-ghiglione-1737081b4?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B%2F%2BvzNNFLQUWG6WT%2BAHW%2Fog%3D%3D"
     class="footer__link"
@@ -24,8 +23,7 @@ function footerComponent(el) {
       alt="Icono linkedIn"
     />LinkedIn</a
   >
-  </div>
-  <div class="footer__content-link">
+ 
   <a href="https://github.com/ghiglionemag" class="footer__link"
     ><img
       class="footer__icon"
@@ -33,7 +31,6 @@ function footerComponent(el) {
       alt="Icono GitHub"
     />GitHub</a
   >
-  </div>
 </div>`;
 
   el.appendChild(componentEl);
