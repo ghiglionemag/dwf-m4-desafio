b6aed0fbb57cc6559a30783f9787d030ee1e2c78
Agustina Ghiglione <ghiglionemag@gmail.com>
2022-03-21 20:50:33 -0300

    Probando algunos cambios para mejorar el diseño

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 function main() {
   const header = document.querySelector(".header");
   headerComponent(header);
-
-  const contact = document.querySelector(".contactame");
-  contactComponent(contact);
-
-  const servicios = document.querySelector(".servicios");
-  serviciosComponent(servicios);
-
   const footer = document.querySelector(".footer");
   footerComponent(footer);
 }
